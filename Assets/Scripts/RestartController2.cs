8f1080818b32e88c96110b170792bb2397e95e49
cf <hoge>
2022-03-04 11:41:07 +0900

    stage2のRestart遷移完了

---
+++ Assets/Scripts/RestartController2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartController2 : MonoBehaviour
+{
+    void Start()
+    {
+        //1.5秒後にReturnToStage2を呼び出す
+        Invoke("ReturnToStage2", 1.5f);
+    }
+    void ReturnToStage2()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+}
+
--- Assets/Scripts/Stage2Controller.cs
+++ Assets/Scripts/Stage2Controller.cs
@@ -19,17 +19,17 @@ public class Stage2Controller : MonoBehaviour
             //これ以降のUpdateは止める
             enabled = false;
 
-            //2秒後にReturnToStage2を呼び出す
-            Invoke("ReturnToStage2", 2.0f);
+            //2秒後にReturnToRestart2を呼び出す
+            Invoke("ReturnToRestart2", 2.0f);
 
         }
 
     }
 
-    void ReturnToStage2()
+    void ReturnToRestart2()
     {
-        //タイトルシーンに切り替え
-        SceneManager.LoadScene("Stage2");
+        //Restart2に切り替え
+        SceneManager.LoadScene("Restart2");
     }
 
     
