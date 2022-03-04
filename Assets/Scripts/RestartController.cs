a5fdbbf3ff47dbb67cd0a1bfc35657739d71ce65
cf <hoge>
2022-03-04 11:24:35 +0900

    RestartController付与

---
+++ Assets/Scripts/RestartController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartController : MonoBehaviour
+{
+    void Start()
+    {
+        //1.5秒後にReturnToStage1を呼び出す
+        Invoke("ReturnToStage1", 1.5f);
+    }
+    void ReturnToStage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+}
